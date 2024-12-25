77b907350bad33749aab94f2c1dc756263ffbfa4
krishna soni <110759309+krishnaSONI011@users.noreply.github.com>
2024-12-25 11:39:20 +0530

    vercel api intrigreted

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello server');
+});
+
+app.post('/send-data', async (req, res) => {
+    try {
+        const formData = req.body; // Extract data from the request body
+        const response = await axios.post('http://3.110.189.42:8000/enquiry/', formData, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.status === 200) {
+            res.status(200).json({
+                status:true,
+                message:"data send"
+            })
+        } else {
+            console.error('Error:', response.statusText);
+            res.status(response.status).send('Failed to send data');
+        }
+    } catch (e) {
+        console.error('Error:', e);
+        res.status(500).send('Internal server error');
+    }
+});
+module.exports = app;
